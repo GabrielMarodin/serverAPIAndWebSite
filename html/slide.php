14bc6bca90a5dc2ef8692ca7f8048c8d0a4c9ea3
GabrielMarodin <gabrielmarodin@hotmail.com>
2019-11-20 04:40:18 -0300

    slideshow works

--- html/slide.php
+++ html/slide.php
@@ -8,14 +8,14 @@
       require_once 'files/getfiles.php';
 
       foreach($data as $file) {
-        echo '<div class="mySlides fade">';
+        echo '<div class="mySlides fade" id="mySlides">';
           echo '<div class="numbertext">'.$file['id'].'</div>';
           if($file['tipo'] == 'mp4' || $file['tipo'] == 'obb' || $file['tipo'] == 'webm'){
-            echo '<video class="responsive-video" autoplay="autoplay" preload="metadata">';
+            echo '<video id="'.$file['id'].'" class="responsive-video" data-duracao="'.$file['duracao'].'" preload="metadata"  muted="muted">';
               echo '<source src="files/'.$file['path'].'" type="video/'.$file['tipo'].'">';
             echo '</video>';
           }else{
-            echo '<img src="files/'.$file['path'].'">';
+            echo '<img id="'.$file['id'].'" data-duracao="30" src="files/'.$file['path'].'">';
           }
         echo '</div>';
       }
--- js/slide.js
+++ js/slide.js
@@ -1,14 +1,42 @@
 var slideIndex = 0;
 showSlides();
 
-function showSlides() {
+function showSlides(){
+
   var i;
+  var j;
+
   var slides = document.getElementsByClassName("mySlides");
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slideIndex++;
+
   if (slideIndex > slides.length) {slideIndex = 1}
+
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+  var child;
+  var id;
+  var element;
+  
+  for(j = 0; j < slides[slideIndex-1].childNodes.length; j++){
+    child = slides[slideIndex-1].childNodes[j];
+    if(child.nodeName == 'VIDEO' || child.nodeName == 'IMG'){
+        id = child.id;
+    }
+  }
+
+  element = document.getElementById(id.toString());
+
+  var length = Number(element.dataset.duracao) * 1000;
+  console.log(length)
+
+  if (element.tagName == 'VIDEO') {
+    element.play(); 
+  }
+
+  setTimeout(showSlides, length);
 }
